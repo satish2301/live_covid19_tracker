13bcb29d4db5f9553f679bf2299dda8ce3a74385
MCA WALE <106832389+satish2301@users.noreply.github.com>
2023-11-09 03:16:58 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App=()=> {
    
     <div className="App">
       <div className='top-main-heading'>
-         <h1><img src={Image}/> LIVE</h1>
+         <h1><img src={Image} alt=""/> LIVE</h1>
          <h2>COVID-19 CORONAVIRUS TRACKER</h2>
       </div>
     </div>
